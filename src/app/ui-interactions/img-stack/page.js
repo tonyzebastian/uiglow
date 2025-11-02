c621c0ec7e30926b5b6b619d195695d5cb27c0ab
tonyzebastian <tonyzebaztian@yahoo.com>
2025-11-02 10:21:07 +0530

    changed images to unsplash

--- src/app/ui-interactions/img-stack/page.js
+++ src/app/ui-interactions/img-stack/page.js
@@ -8,11 +8,11 @@ export const metadata = {
 
 export default function ImgStackPage() {
   const imageUrls = [
-    'https://res.cloudinary.com/dctgknnt7/image/upload/v1758731403/1_d8uozd.jpg',
-    'https://res.cloudinary.com/dctgknnt7/image/upload/v1758731402/5_ionpyy.jpg',
-    'https://res.cloudinary.com/dctgknnt7/image/upload/v1758731402/4_zeoqje.jpg',
-    'https://res.cloudinary.com/dctgknnt7/image/upload/v1758731402/2_hme6yu.jpg',
-    'https://res.cloudinary.com/dctgknnt7/image/upload/v1758731402/3_nfdtim.jpg'
+    'https://images.unsplash.com/photo-1742201877377-03d18a323c18?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1064',
+    'https://images.unsplash.com/photo-1757081791153-3f48cd8c67ac?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=987',
+    'https://images.unsplash.com/photo-1757626961383-be254afee9a0?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=987',
+    'https://images.unsplash.com/photo-1756748371390-099e4e6683ae?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=987',
+    'https://images.unsplash.com/photo-1755884405235-5c0213aa3374?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=987'
   ];
 
   return (
--- src/app/ui-interactions/img-tiles/page.js
+++ src/app/ui-interactions/img-tiles/page.js
@@ -11,9 +11,9 @@ export default function ImgTile() {
     <CenteredPageLayout>
       <div className='-ml-12'>  {/* Removed -ml-12 */}
         <ImageReveal
-            leftImage="/ui/imgtiles/left.jpg"
-            middleImage="/ui/imgtiles/middle.jpg"
-            rightImage="/ui/imgtiles/right.jpg"
+            leftImage="https://images.unsplash.com/photo-1758178309498-036c3d7d73b3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=987"
+            middleImage="https://images.unsplash.com/photo-1757647016230-d6b42abc6cc9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2072"
+            rightImage="https://images.unsplash.com/photo-1757906447358-f2b2cb23d5d8?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=987"
         />
       </div>
     </CenteredPageLayout>
